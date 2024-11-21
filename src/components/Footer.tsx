929e79332be3cbae54b2793551210283cd5dedcf
WhidroxDash <sergioprieto727@gmail.com>
2024-11-21 14:51:49 -0500

    Se agrego informacion basica

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <p>&copy; 2024 Gestión Documental. Todos los derechos reservados.</p>
-      <p>Contacto: soporte@gestiondocumental.com | Tel: +1 234 567 890</p>
+      <p>Contacto: soporte@gestiondocumental.com | Tel: +0000000</p>
+      <p>Sergio Alexander Prieto Castiblanco | Estudiante de Ingenieria de Sistemas</p>
     </footer>
   );
 };
